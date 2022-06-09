27aca65ff8df008c2a0483d7fed30500b0b7114c
josephLanzasLopez <josephlanzas4@gmail.com>
2022-06-09 17:33:08 -0600

    Trabajo  de tarea 2  de joseph lanzas lopez

---
+++ src/main/java/com/TareaSemana4/TareaSemana4Application.java
@@ -0,0 +1,13 @@
+package com.TareaSemana4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaSemana4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaSemana4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/TareaSemana4/TareaSemana4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TareaSemana4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaSemana4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
